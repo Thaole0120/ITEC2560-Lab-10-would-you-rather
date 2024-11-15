066012985d006ee6742df8806ebc8ceae8df6c35
Thaole <qx6771tj@go.minneapolis.edu>
2024-11-14 23:15:06 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<script setup>
+import WouldYouRather from './components/WouldYouRather.vue'
+
+import { ref } from 'vue'
+
+
+const wyrQuestion = ref('Live in a house shaped like a triangle or a house shaped like a circle?')
+const wyrAnswer1 = ref('Triangle house')
+const wyrAnswer2 = ref('Circle house')
+
+// this will store the user's answer once they made a choice
+
+const userSelection = ref('')
+
+function updateUserSelection(userChoice){
+  userSelection.value = `Thanks! you chose ${userChoice}`
+}
+
+
+
+</script>
+
+<template>
+  <div id="app-component">
+
+  <h1>Would You Rather</h1>
+
+
+  <WouldYouRather 
+    v-bind:question="wyrQuestion" 
+    v-bind:answer1="wyrAnswer1" 
+    v-bind:answer2="wyrAnswer2"
+    v-on:answer-selected="updateUserSelection"
+
+  ></WouldYouRather>
+
+
+  <p>{{ userSelection }}</p>
+
+</div>
+
+</template>
+
+<style scoped>
+
+
+#app-component {
+ /*font-size: 3em; */
+ background-color: lightpink;
+ padding: 40px;
+
+}
+
+p {
+  font-family: 'Courier New', Courier, monospace ;
+}
+
+
+
+</style>
---
+++ src/components/WouldYouRather.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref, watch } from 'vue'
+
+defineProps({
+  question: String,
+  answer1: String,
+  answer2: String
+})
+
+const emit = defineEmits([
+  'answer-selected'
+])
+
+const choice = ref('')
+
+// function choiceMade(){
+//   //emit event to tell parent that user has made choice. 
+//   emit('answer-selected', choice.value)
+// }
+
+watch(choice, ()=> {
+  emit('answer-selected', choice.value)
+})
+  
+
+</script>
+
+<template>
+  <div id="wyr">
+    <h2> Make your choice!</h2>
+
+    <p>{{ question }}</p>
+
+    <div>
+    <input v-model="choice" v-bind:value="answer1"  type="radio" id="answer-1">
+    <label for="answer-1">{{ answer1 }}</label>
+
+    <!-- this value attribute is the HTML value, for what data an input store-->
+    <input v-model="choice" v-bind:value="answer2" type="radio" id="answer-2">
+    <label for="answer-2">{{ answer2 }}</label>
+    </div>
+</div>
+
+</template>
+
+<style scoped>
+#wyr {
+  background-color: lightskyblue;
+}
+
+p {
+  font-family: fantasy;
+}
+
+</style>
